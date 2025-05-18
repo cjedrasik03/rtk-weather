ee71549228bb92e927e9ca83446778584517c0d5
chris jedrasik <chris.jed03@gmail.com>
2025-05-17 21:18:58 -0400

    pure pain, store is setup (i think), my bootstrap is styling and have no clue why. Will get the input value and run that to the API next

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,16 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'RTK-Weather App',
-}
+'use client';
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { Provider } from 'react-redux';
+import store from './store/store';
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang='en'>
+			<body className={inter.className}>
+				<Provider store={store}>{children}</Provider>
+			</body>
+		</html>
+	);
 }
--- app/page.js
+++ app/page.js
@@ -1,55 +1,66 @@
+'use client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [error, setError] = useState('');
+
+  // this detemines what the submit button does and under what conditions
+const handleSearch = (e) => {
+
+  // this prevents the page from reseting
+  e.preventDefault();
+};
+  const handleChange = (e) => { 
+    setInputValue(e.target.value);
+    console.log(inputValue);
+}
   return (
     <>
-      <div className='pt-5'>
-        <div>
-        <Form className='col-4 pb-5 align-items-center container-lg'>
-          <Form.Control size="lg" type="text" placeholder="Search a city" />
-          <Button className="col-12 d-grid mt-2" variant="primary" size="lg">
-             Search
-           </Button>
-        </Form>
-        </div>
+      {/* Primary background */}
+      <Container fluid className="primaryBackground min-vh-100 py-5">
+        
+        {/* Search box centered */}
+        <Row className="justify-content-center mb-4">
+          <Col xs={12} md={6} lg={6}>
+            <div className="bg-primary-subtle border border-primary-subtle rounded-4 p-4">
+                <Row className="g-2 align-items-center">
+                  <Col xs={12} md={9}>
+                    <input
+                      size="lg"
+                      type="text"
+                      placeholder="Search a city"
+                      value={inputValue}
+                      onChange={handleChange}
+                    />
+                  </Col>
+                  <Col xs={12} md={3}>
+                    <button className="w-100" variant="primary" size="lg">
+                      Search
+                    </button>
+                  </Col>
+                </Row>
+            </div>
+          </Col>
+        </Row>
 
-        <div className='col-6 align-items-center container-lg p-1'>
-          <Container className='bg-primary-subtle border border-primary-subtle rounded-4'>
-            <div className='p-2 text-center'>City Title 1</div>
-            <Row className='d-flex justify-content-center align-items-center'>
-              <Col className='m-3'>Temp.</Col>
-              <Col className='m-3'>Hum.</Col>
-              <Col className='m-3'>Pres.</Col>
-            </Row>
-          </Container>
-        </div>
-
-        <div className='col-6 align-items-center container-lg p-1'>
-        <Container className='bg-primary-subtle border border-primary-subtle rounded-4'>
-          <div className='p-2 text-center'>City Title 2</div>
-          <Row className='d-flex justify-content-center align-items-center'>
-            <Col className='m-3'>Temp.</Col>
-            <Col className='m-3'>Hum.</Col>
-            <Col className='m-3'>Pres.</Col>
-          </Row>
-        </Container>
-      </div>
-
-      <div className='col-6 align-items-center container-lg p-1'>
-        <Container className='bg-primary-subtle border border-primary-subtle rounded-4'>
-          <div className='p-2 text-center'>City Title 3</div>
-          <Row className='d-flex justify-content-center align-items-center'>
-            <Col className='m-3'>Temp.</Col>
-            <Col className='m-3'>Hum.</Col>
-            <Col className='m-3'>Pres.</Col>
-          </Row>
-        </Container>
-      </div>
-    </div>
-  </>    
-  )
+        {/* Forecast boxes */}
+        <Row className="justify-content-center">
+          <Col md={8}>
+            <Container className="bg-primary-subtle border border-primary-subtle rounded-4 p-3 mb-2">
+              <div className="p-2 text-center">City Title 1</div>
+              <Row className="d-flex justify-content-center align-items-center">
+                <Col className="m-3">Temp.</Col>
+                <Col className="m-3">Hum.</Col>
+                <Col className="m-3">Pres.</Col>
+              </Row>
+            </Container>
+          </Col>
+          {/* Add more forecast boxes as needed */}
+        </Row>
+      </Container>
+    </>
+  );
 }
